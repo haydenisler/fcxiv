cda92df74a928b24a5c1b47377011bc16d76741d
Hayden Isler <me@haydenisler.com>
2022-04-09 01:11:13 -0500

    feat: add injectable styles to avatar

--- src/common/components/Avatar/Avatar.tsx
+++ src/common/components/Avatar/Avatar.tsx
@@ -1,9 +1,18 @@
 import type { FC } from 'react';
+import clsx from 'clsx';
 
 export interface AvatarProps {
   src?: string;
+  className?: string;
 }
 
-export const Avatar: FC<AvatarProps> = ({ src }) => {
-  return <div className="w-8 h-8 bg-gray-100 border border-gray-300 rounded-full" />;
+export const Avatar: FC<AvatarProps> = ({ src, className }) => {
+  return ( 
+    <div 
+      className={clsx(
+        "w-8 h-8 bg-gray-100 border border-gray-300 rounded-full",
+        className
+      )} 
+    />
+  );
 }
