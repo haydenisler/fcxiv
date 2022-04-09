fc2e7dc484b38224ad157e5d12adf259f59184bc
Hayden Isler <me@haydenisler.com>
2022-04-08 20:08:42 -0500

    refactor: nav -> Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,13 @@
 import '../common/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { supabase } from '../config/supabase.config';
-import { useEffect, useState } from 'react';
-import { Session } from '@supabase/supabase-js';
-import { DiscordSignIn } from '../features/auth';
-import { Avatar } from '../common/components';
+import { HamburgerToggle, Header } from '../common/components';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    setSession(supabase.auth.session());
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
   return(
-    <div>
-      <nav className="flex items-center justify-between px-6 py-4 border-b border-gray-300">
-        <h1 className="text-lg font-semibold text-indigo-600">fcxiv</h1>
-        {session ? (
-          <Avatar />
-        ) : (
-          <DiscordSignIn />
-        )}
-      </nav>
+    <div className="relative min-h-screen">
+      <Header />
       <Component {...pageProps} />
-
+      <HamburgerToggle />
     </div>
   );
 };
